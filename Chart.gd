2deea097848b5d26326d38daf36766a9402f9aa0
philipp <s0554213@htw-berlin.de>
2020-05-23 16:51:01 +0200

    api for charts added

--- Chart.gd
+++ Chart.gd
@@ -1,34 +1,53 @@
 extends Node2D
 
 const axis_label = preload("res://AxisLabel.tscn")
-const CANVAS_SIZE = 500
-const YMIN = 60
-const YMAX = 80
-const YSTEP = 1
-const XMIN = 0
-const XMAX = 900
-const XSTEP = 100
-var cols
-var rows
-var cell_size
+const CANVAS_SIZE: int = 500
+var cols: int = 0
+var rows: int = 0
+var cell_size: float
+var i = 0
+var initial_pos : Vector2
+var labels : Array
 
 func _ready():
-	rows = (YMAX - YMIN) / YSTEP
-	cols = (XMAX - XMIN) / XSTEP
+	initial_pos = position
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if i == 0:
+			draw_chart(0, 100, 10, 2, 23, 49, 1, 2, [[]])
+			i+=1
+		else:
+			draw_chart(0, 200, 10, 4, 0.2, 0.8, 0.05, 2, [[]])
+		
+func draw_chart(x_min: float, x_max: float, x_step: float, x_label_every: int, y_min: float, y_max: float, y_step: float, y_label_every: int, data: Array):
+	if len(labels) > 0:
+		for l in labels:
+			l.queue_free()
+		labels.clear()
+			
+	cols = (x_max - y_min) / x_step
+	rows = (y_max - y_min) / y_step
 	cell_size = CANVAS_SIZE / max(rows, cols)
-	position -= Vector2((cols * cell_size) / 2, (rows * cell_size) / 2)
+	position = initial_pos - Vector2((cols * cell_size) / 2, (rows * cell_size) / 2)
 	
 	for c in range(cols + 1):
-		var label = axis_label.instance()
-		label.get_node("Label").text = str(XMIN + XSTEP * c)
-		label.position = Vector2(cell_size * c, cell_size * rows + 20) 
-		add_child(label)
+		if c % 2 == 0:
+			var label = axis_label.instance()
+			label.get_node("Label").text = str(x_min + x_step * c)
+			label.position = Vector2(cell_size * c, cell_size * rows + 20)
+			labels.append(label)
+			add_child(label)
 		
 	for r in range(rows + 1):
-		var label = axis_label.instance()
-		label.get_node("Label").text = str(YMIN + YSTEP * (rows - r))
-		label.position = Vector2(-20 , cell_size * r) 
-		add_child(label)
+		if r % 2 == 0:
+			var label = axis_label.instance()
+			label.get_node("Label").text = str(y_min + y_step * (rows - r))
+			label.position = Vector2(-20 , cell_size * r)
+			labels.append(label)
+			add_child(label)
+	
+	update()
 
 func _draw():
 	for c in range(cols + 1):
