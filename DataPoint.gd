5e35cb86ac15ba352822d8886e6fc28e641309f3
philipp <s0554213@htw-berlin.de>
2020-06-06 00:17:38 +0200

    tool tips wip

---
+++ DataPoint.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var car_preview = get_node("/root/Main/CarPreview")
+onready var car_description = get_node("/root/Main/CarDescription")
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		car_description.visible = true
+		if global_position.y > 540:
+			car_description.position = global_position - Vector2(0, 125)
+		else:
+			car_description.position = global_position + Vector2(0, 125)
+
+
+func _on_Area2D_mouse_entered():
+	car_preview.visible = true
+	if global_position.y > 540:
+		car_preview.position = global_position - Vector2(0, 90)
+	else:
+		car_preview.position = global_position + Vector2(0, 90)
+
+func _on_Area2D_mouse_exited():
+	car_preview.visible = false
